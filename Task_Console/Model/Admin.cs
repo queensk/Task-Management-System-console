654b9915465a9f70654d73f8605cf7d59a0b79cc
queens kisivuli <queenskisivuli@gmail.com>
2023-08-27 16:19:22 +0300

    user admin functions

--- Task_Console/Model/Admin.cs
+++ Task_Console/Model/Admin.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Task_Console.Model
 {
-    public class Admin
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
+public class Admin
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Username { get; set; }
+    public string Password { get; set; }
+     public ICollection<Project> Projects { get; set; } = new List<Project>();
+}
 }
